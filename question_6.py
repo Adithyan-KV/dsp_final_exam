d41255831b77ae50fa297675df6c9029bc982801
Adithyan KV <adithyankv007@gmail.com>
2021-01-29 23:38:11 +0530

    Major bug fix
     -The previous EAHM and UAHM were horribly wrong
     -Fixed them now
     -a to d done. looks right (fingers crossed)

--- question_6.py
+++ question_6.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+import scipy.fftpack as fft
 
 def main():
     # all SI units
@@ -10,99 +11,165 @@ def main():
     test_signal = np.zeros(length)
     speech_signal = (wavfile.read('speech.wav'))[1]
 
+    # #---------------------------------------------------
+    # # Question a
+    # #---------------------------------------------------
+
+    # EAHN_1 = get_eahn(test_signal)
+    # EAHN_2 = get_eahn(test_signal)
+    # EAHN_3 = get_eahn(test_signal)
+    # EAHN_4 = get_eahn(test_signal)
+
+    # # Plotting EAHN
+    # fig, plots = plt.subplots(2,2)
+    # fig.suptitle("4 realizations of EAHN")
+    # plots[0,0].plot(EAHN_1)
+    # plots[0,0].set_xlabel('n')
+    # plots[0,0].set_ylabel('e[n]')
+    # plots[0,1].plot(EAHN_2)
+    # plots[0,1].set_xlabel('n')
+    # plots[0,1].set_ylabel('e[n]')
+    # plots[1,0].plot(EAHN_3)
+    # plots[1,0].set_xlabel('n')
+    # plots[1,0].set_ylabel('e[n]')
+    # plots[1,1].plot(EAHN_4)
+    # plots[1,1].set_xlabel('n')
+    # plots[1,1].set_ylabel('e[n]')
+    # plt.show()
+
+    # #---------------------------------------------------
+    # # Question b
+    # #---------------------------------------------------
+
+    # UAHN_1 = get_uahn(test_signal)
+    # UAHN_2 = get_uahn(test_signal)
+    # UAHN_3 = get_uahn(test_signal)
+    # UAHN_4 = get_uahn(test_signal)
+
+    # # Plotting UAHN
+    # fig, plots = plt.subplots(2,2)
+    # fig.suptitle("4 realizations of UAHN")
+    # plots[0,0].plot(UAHN_1)
+    # plots[0,0].set_xlabel('n')
+    # plots[0,0].set_ylabel('e[n]')
+    # plots[0,1].plot(UAHN_2)
+    # plots[0,1].set_xlabel('n')
+    # plots[0,1].set_ylabel('e[n]')
+    # plots[1,0].plot(UAHN_3)
+    # plots[1,0].set_xlabel('n')
+    # plots[1,0].set_ylabel('e[n]')
+    # plots[1,1].plot(UAHN_4)
+    # plots[1,1].set_xlabel('n')
+    # plots[1,1].set_ylabel('e[n]')
+    # plt.show()
+
+    # #---------------------------------------------------
+    # # Question c
+    # #---------------------------------------------------
+    # EAHN_1 = get_eahn(speech_signal)
+    # EAHN_2 = get_eahn(speech_signal)
+    # db = [0,5,10,15]
+    # fig, plots = plt.subplots(4,2)
+    # fig.suptitle("EAHN added at different SNRs")
+    # for index,snr in enumerate(db):
+    #     noisy_1 = add_noise_to_signal(speech_signal, EAHN_1, snr)
+    #     noisy_2 = add_noise_to_signal(speech_signal, EAHN_2, snr)
+    #     plots[index,0].plot(noisy_1)
+    #     plots[index,0].set_title(f'First realization of noisy signal with SNR:{snr}')
+    #     plots[index,0].xaxis.set_visible(False)
+    #     plots[index,1].plot(noisy_2)
+    #     plots[index,1].set_title(f'Second realization of noisy signal with SNR:{snr}')
+    #     plots[index,1].xaxis.set_visible(False)
+    # plt.show()
+
+    # #---------------------------------------------------
+    # # Question d
+    # #---------------------------------------------------
+    # UAHN_1 = get_uahn(speech_signal)
+    # UAHN_2 = get_uahn(speech_signal)
+    # db = [0,5,10,15]
+    # fig, plots = plt.subplots(4,2)
+    # fig.suptitle("UAHN added at different SNRs")
+    # for index,snr in enumerate(db):
+    #     noisy_1 = add_noise_to_signal(speech_signal, UAHN_1, snr)
+    #     noisy_2 = add_noise_to_signal(speech_signal, UAHN_2, snr)
+    #     plots[index,0].plot(noisy_1)
+    #     plots[index,0].set_title(f'First realization of noisy signal with SNR:{snr}')
+    #     plots[index,0].xaxis.set_visible(False)
+    #     plots[index,1].plot(noisy_2)
+    #     plots[index,1].set_title(f'Second realization of noisy signal with SNR:{snr}')
+    #     plots[index,1].xaxis.set_visible(False)
+    # plt.show()
+
     #---------------------------------------------------
-    # Question a
+    # Question e
     #---------------------------------------------------
+    EAHN_1 = get_eahn(speech_signal)
+    EAHN_2 = get_eahn(speech_signal)
+    # for 0 db noise
+    noisy_signal = add_noise_to_signal(EAHN_1, speech_signal, 5)
+    fft_signal = fft.fft(noisy_signal)
+    freq = fft.fftfreq(len(speech_signal),d=1/sampling_rate)
+    power_spectrum = fft_signal*np.conjugate(fft_signal)
+    # plt.plot(freq, np.abs(power_spectrum))
+    # plt.scatter(np.arange(-3600,3600,600),np.zeros(12),color='red')
+    # plt.show()
+    power_noise = 0
+    for harmonic_freq in range(600,4000,600):
+        power_noise += np.abs(np.sum(power_spectrum[harmonic_freq-2:harmonic_freq+3]))
+    power_signal = np.abs(np.sum(power_spectrum)-2*power_noise)
+    snr = 10*np.log10(power_signal/power_noise)
+    print(snr)
 
-    EAHN_1 = e(test_signal, 1)
-    EAHN_2 = e(test_signal, 1)
-    EAHN_3 = e(test_signal, 1)
-    EAHN_4 = e(test_signal, 1)
-
-    # Plotting EAHN
-    fig, plots = plt.subplots(2,2)
-    fig.suptitle("4 realizations of EAHN")
-    plots[0,0].plot(EAHN_1)
-    plots[0,1].plot(EAHN_2)
-    plots[1,0].plot(EAHN_3)
-    plots[1,1].plot(EAHN_4)
-    plt.show()
-
-    #---------------------------------------------------
-    # Question b
-    #---------------------------------------------------
-
-    A_values = np.random.uniform(0,1,length)
-    # setting A_1=1
-    A_values[0]=1
-    UAHN_1 = e(test_signal, A_values)
-    UAHN_2 = e(test_signal, A_values)
-    UAHN_3 = e(test_signal, A_values)
-    UAHN_4 = e(test_signal, A_values)
-
-    # Plotting UAHN
-    fig, plots = plt.subplots(2,2)
-    fig.suptitle("4 realizations of UAHN")
-    plots[0,0].plot(UAHN_1)
-    plots[0,1].plot(UAHN_2)
-    plots[1,0].plot(UAHN_3)
-    plots[1,1].plot(UAHN_4)
-    plt.show()
-
-    #---------------------------------------------------
-    # Question c
-    #---------------------------------------------------
-    A_values = np.random.uniform(0,1,len(speech_signal))
-    A_values[0]=1
-    EAHN_1 = e(speech_signal, A_values)
-    EAHN_2 = e(speech_signal, A_values)
-    db = [0,5,10,15]
-    fig, plots = plt.subplots(4,2)
-    fig.suptitle("EAHN added at different SNRs")
-    for index,snr in enumerate(db):
-        noisy_1 = add_noise_to_signal(speech_signal, EAHN_1, snr)
-        noisy_2 = add_noise_to_signal(speech_signal, EAHN_2, snr)
-        plots[index,0].plot(noisy_1)
-        plots[index,0].set_title(f'First realization of noisy signal with SNR:{snr}')
-        plots[index,0].xaxis.set_visible(False)
-        plots[index,1].plot(noisy_2)
-        plots[index,1].set_title(f'Second realization of noisy signal with SNR:{snr}')
-        plots[index,1].xaxis.set_visible(False)
-    plt.show()
-
-    #---------------------------------------------------
-    # Question d
-    #---------------------------------------------------
-    A_values = np.ones(len(speech_signal))
-    UAHN_1 = e(speech_signal, A_values)
-    UAHN_2 = e(speech_signal, A_values)
-    db = [0,5,10,15]
-    fig, plots = plt.subplots(4,2)
-    fig.suptitle("UAHN added at different SNRs")
-    for index,snr in enumerate(db):
-        noisy_1 = add_noise_to_signal(speech_signal, UAHN_1, snr)
-        noisy_2 = add_noise_to_signal(speech_signal, UAHN_2, snr)
-        plots[index,0].plot(noisy_1)
-        plots[index,0].set_title(f'First realization of noisy signal with SNR:{snr}')
-        plots[index,0].xaxis.set_visible(False)
-        plots[index,1].plot(noisy_2)
-        plots[index,1].set_title(f'Second realization of noisy signal with SNR:{snr}')
-        plots[index,1].xaxis.set_visible(False)
-    plt.show()
+    # #---------------------------------------------------
+    # # Question f
+    # #---------------------------------------------------
+    # UAHN_1 = get_uahn(speech_signal)
+    # UAHN_2 = get_uahn(speech_signal)
+    # # for 0 db noise
+    # noisy_signal = add_noise_to_signal(UAHN_1, speech_signal, 15)
+    # fft_signal = fft.fft(noisy_signal)
+    # # freq = fft.fftfreq(len(noisy_signal),d=1/sampling_rate)
+    # power_spectrum = fft_signal*np.conjugate(fft_signal)
+    # # plt.plot(freq, np.abs(power_spectrum))
+    # # plt.scatter(np.arange(120,1000,120),np.zeros(8),color='red')
+    # # plt.show()
+    # power_noise = 0
+    # for harmonic_freq in range(600,4000,600):
+    #     power_noise += np.abs(np.sum(power_spectrum[harmonic_freq-1:harmonic_freq+1]))
+    # power_signal = np.abs(np.sum(power_spectrum)-2*power_noise)
+    # snr = 10*np.log10(power_signal/power_noise)
+    # print(snr)
 
 
-def e(n, Ak, f0=600):
-    length = len(n)
-    k = np.arange(0,length,1)
-    phi_k = np.random.uniform(0,2*np.pi,length)
-    series = Ak*np.sin(2*np.pi*k*n+phi_k)
-    en = np.cumsum(series)
-    return en
+
+def get_eahn(signal,f0=600,Fs=8000):
+    N = len(signal)
+    K = 6
+    Ak = 1
+    e = np.zeros(N)
+    phi = np.random.uniform(0,2*np.pi,K)
+    for n in range(N):
+        for k in range(K):
+            e[n]+=Ak*np.sin(2*np.pi*k*f0/Fs*n+phi[k])
+    return e
+
+def get_uahn(signal,f0=600,Fs=8000):
+    N = len(signal)
+    K = 6
+    Ak = np.random.uniform(0,1,K)
+    Ak[0]=1
+    e = np.zeros(N)
+    phi = np.random.uniform(0,2*np.pi,K)
+    for n in range(N):
+        for k in range(K):
+            e[n]+=Ak[k]*np.sin(2*np.pi*k*f0/Fs*n+phi[k])
+    return e
 
 def get_constant_for_snr(signal, noise, snr):
     Es = np.sum(np.square(signal))
     En = np.sum(np.square(noise))
-    constant = (Es/En)*10**(-snr/10)
+    constant = (Es/En)*(10**(-snr/10))
     return constant
 
 def add_noise_to_signal(signal, noise, snr):
